af45fffd0732a705164ed79ce648f4fc19ba4164
cinyee1214 <xinyizhao1214@gmail.com>
2020-10-12 22:44:15 -0400

    database

--- app/src/main/java/com/laioffer/tinnews/TinNewsApplication.java
+++ app/src/main/java/com/laioffer/tinnews/TinNewsApplication.java
@@ -2,17 +2,26 @@ package com.laioffer.tinnews;
 
 import android.app.Application;
 
+import androidx.room.Room;
+
 import com.ashokvarma.gander.Gander;
 import com.ashokvarma.gander.imdb.GanderIMDB;
+import com.laioffer.tinnews.database.TinNewsDatabase;
 
 public class TinNewsApplication extends Application {
-
+    private TinNewsDatabase database;
 
     @Override
     public void onCreate() {
         super.onCreate();
         Gander.setGanderStorage(GanderIMDB.getInstance());
-        // TODO: new code here.
+       // Stetho.initializeWithDefaults(this);
+        database = Room.databaseBuilder(this, TinNewsDatabase.class, "tinnews_db").build();
     }
+
+    public TinNewsDatabase getDatabase() {
+        return database;
+    }
+
 }
 
--- app/src/main/java/com/laioffer/tinnews/model/Article.java
+++ app/src/main/java/com/laioffer/tinnews/model/Article.java
@@ -1,7 +1,12 @@
 package com.laioffer.tinnews.model;
 
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import java.util.Objects;
 
+@Entity
 public class Article {
     public String author;
     public String content;
@@ -41,6 +46,8 @@ public class Article {
 
     public String publishedAt;
     public String title;
+    @NonNull
+    @PrimaryKey
     public String url;
     public String urlToImage;
 }
