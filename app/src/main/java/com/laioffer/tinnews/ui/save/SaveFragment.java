da7fe848059ec71fec6b086bb20cbd84b06d4dd2
cinyee1214 <xinyizhao1214@gmail.com>
2020-10-14 22:13:19 -0400

    view saved_news_item.xml

--- app/src/main/java/com/laioffer/tinnews/ui/save/SaveFragment.java
+++ app/src/main/java/com/laioffer/tinnews/ui/save/SaveFragment.java
@@ -2,15 +2,24 @@ package com.laioffer.tinnews.ui.save;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.laioffer.tinnews.R;
+import com.laioffer.tinnews.databinding.FragmentSaveBinding;
+import com.laioffer.tinnews.repository.NewsRepository;
+import com.laioffer.tinnews.repository.NewsViewModelFactory;
 
 public class SaveFragment extends Fragment {
+    private FragmentSaveBinding binding;
+    private SaveViewModel viewModel;
 
     public SaveFragment() {
         // Required empty public constructor
@@ -25,6 +34,25 @@ public class SaveFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_save, container, false);
+        binding =  FragmentSaveBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NewsRepository repository = new NewsRepository(getContext());
+        viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository)).get(SaveViewModel.class);
+        viewModel
+                .getAllSavedArticles()
+                .observe(
+                        getViewLifecycleOwner(),
+                        savedArticles -> {
+                            if (savedArticles != null) {
+                                Log.d("SaveFragment", savedArticles.toString());
+                            }
+                        });
     }
 }
