49748d447b999fdc1fb36b777b5ddbd6d12d1292
cinyee1214 <xinyizhao1214@gmail.com>
2020-10-17 12:34:09 -0400

    Details Page Implementation

---
+++ app/src/main/java/com/laioffer/tinnews/ui/details/DetailsFragment.java
@@ -0,0 +1,34 @@
+package com.laioffer.tinnews.ui.details;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.laioffer.tinnews.R;
+import com.laioffer.tinnews.databinding.FragmentDetailsBinding;
+
+public class DetailsFragment extends Fragment {
+    private FragmentDetailsBinding binding;
+
+    public DetailsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentDetailsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+}
